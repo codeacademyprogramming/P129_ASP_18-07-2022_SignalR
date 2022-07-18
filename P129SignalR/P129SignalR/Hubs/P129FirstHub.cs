f84b52a658ee8ca4c737c0c56d450200baffe433
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-07-18 12:50:51 +0400

    Second Commit

---
+++ P129SignalR/P129SignalR/Hubs/P129FirstHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P129SignalR.Hubs
+{
+    public class P129FirstHub : Hub
+    {
+        public async Task MesajGonder(string isdifadeci, string mesaji)
+        {
+            await Clients.All.SendAsync("mesajqebuleden", isdifadeci, mesaji, DateTime.UtcNow.AddHours(4).ToString("dd.MM.yyyy HH:mm"));
+        }
+    }
+}
---
+++ P129SignalR/P129SignalR/wwwroot/js/realtime.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    var p129connection = new signalR.HubConnectionBuilder().withUrl('/p129hublink').build();
+
+    p129connection.start();
+
+    $(document).on('click', '#sendButton', function () {
+        let user = $('#userInput').val();
+        let message = $('#messageInput').val();
+
+        p129connection.invoke('MesajGonder', user, message)
+    })
+
+    p129connection.on('mesajqebuleden', function myfunction(str1, str2, date) {
+        let listItem = `<li>${str1} Says: ${str2} ${date}</li>`;
+        $('#messagesList').append(listItem);
+    })
+})
