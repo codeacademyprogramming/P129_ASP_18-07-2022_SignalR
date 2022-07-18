86f2ba17dea58d60a981b138feb410b3ff84773f
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-07-18 12:47:10 +0400

    First Commit

---
+++ P129Allup/P129Allup/wwwroot/assets/js/realtime.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    var connection = new signalR.HubConnectionBuilder().withUrl('/chathub').build();
+
+    connection.start();
+
+    connection.on('online', function (id) {
+        let span = $(`#${id} span`);
+        span.addClass('bg-success');
+        span.removeClass('bg-secondary');
+    })
+
+    connection.on('ofline', function (id) {
+        let span = $(`#${id} span`);
+        span.removeClass('bg-success');
+        span.addClass('bg-secondary');
+    })
+
+    $(document).on('click', '.userItem', function () {
+        
+
+        let fullName = $(this).attr("data-userName")
+        let userId = $(this).attr('id');
+        console.log(`${fullName} ${userId}`);
+        $('.sender').text(fullName);
+        $('#userId').val(userId);
+    })
+})
